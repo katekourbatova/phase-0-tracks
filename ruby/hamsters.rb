8af452062f44e0e18d9618cb849cfca60fb640cf
Kate Kourbatova <ekateko@uw.edu>
2016-10-05 14:24:28 -0700

    Add boolean conversion for adoption question

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -7,8 +7,15 @@ volume=gets.chomp.to_i
 puts "What is its color?"
 color=gets.chomp
 
-puts "Is it a good candidate for adoption?"
-adopt=gets.chomp
+puts "Is it a good candidate for adoption? Y or N"
+adopt=gets.chomp.upcase
+  if adopt == "Y"
+    adopt = true
+  elsif adopt == "N"
+    adopt = false
+  else
+    print "Next time, please enter Y or N. For now, we are leaving this as: #{adopt}. Thank you."
+  end
 
 puts "What is its age?"
 age=gets.chomp.to_f
