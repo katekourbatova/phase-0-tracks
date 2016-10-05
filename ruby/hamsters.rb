e63ca206fd058a407be9db1d7d26e7112a102e9a
Kate Kourbatova <ekateko@uw.edu>
2016-10-05 14:57:47 -0700

    Add message that prints out hamster data at the end, with conditional logic for various things the user may have entered.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,5 +1,5 @@
 puts "What is the hamsters name?"
-name=gets.chomp
+name=gets.chomp.capitalize
 
 puts "What is the volume level?"
 volume=gets.chomp.to_i
@@ -14,11 +14,27 @@ adopt=gets.chomp.upcase
   elsif adopt == "N"
     adopt = false
   else
-    print "Next time, please enter Y or N. For now, we are leaving this as: #{adopt}. Thank you."
+    puts "Next time, please enter Y or N. For now, we are leaving this as: #{adopt}. Thank you."
   end
 
-puts "What is its age?"
-age=gets.chomp.to_f
+puts "What is its age? Please enter in years as a numeral (e.g. 7)"
+age=gets.chomp
 	if age == ""
 		age = nil
+  else
+    age = age.to_f
+  end
+
+  if age == 0.0
+    puts "Next time, please enter the age as a numeral instead of spelling it out."
 	end
+
+print "Thank you for filling in the data for our new hamster, #{name}. "
+print "#{name} has a volume level of #{volume}, and has fur that is a #{color} color. "
+print "Is it a good candidate for adoption? #{adopt} "
+
+  if age == nil
+    print "We're not sure how old #{name} is."
+  else
+    print "#{name} is #{age} years old."
+  end
