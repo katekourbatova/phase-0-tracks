f947f9e47d7fdebc576f9eb8374be2967e4ce818
Kate Kourbatova <ekateko@uw.edu>
2016-10-07 10:38:26 -0700

    Add a questionaire for new employees, release 1.

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you? Please enter a number in years."
+age = gets.chomp.to_i
+puts "What year were you born? Please enter a number."
+yearBorn = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? Y/N"
+likesGarlic = gets.chomp.upcase
+puts "Would you like to enroll in the company's health insurance? Y/N"
+insurance = gets.chomp.upcase
