a2f4a2fbfc393ed14f1b55f9af8b4a5cad79f841
Kate Kourbatova <ekateko@uw.edu>
2016-10-12 15:36:06 -0700

    Add .each and .map! methods to iterate through the array and hash. Release 1.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,7 +1,53 @@
-def define
-  puts "Not yet run"
-  yield(4, 6)
-  puts "I just ran it!!"
+# Release 0
+# def define
+#   puts "Not yet run"
+#   yield(4, 6)
+#   puts "I just ran it!!"
+# end
+
+# define {|par1, par2| puts par1 + par2  }
+
+pasta_ingredients = ["pasta", "marinara sauce", "bell pepper", "onion", "garlic", "tomato"]
+
+guests_diet_restrictions = {
+  "Amy" => "vegetarian",
+  "Brandon" => "lactose intolerant",
+  "Caroline" => "no eggs",
+  "Daryl" => "no peanuts"
+}
+
+
+#iterate through pasta ingredients array
+puts "Pasta ingredients before .each:"
+print pasta_ingredients
+
+pasta_ingredients.each do |ingredient|
+  puts "#{ingredient} is delicious!"
 end
 
-define {|par1, par2| puts par1 + par2  }
+puts "Pasta ingredients after .each:"
+print pasta_ingredients
+
+
+#iterate through guests' diet restriction hash
+puts "Guests' diet restrictions before .each:"
+print guests_diet_restrictions
+
+guests_diet_restrictions.each do |person, restriction|
+  puts "#{person} has this dietary restriction: #{restriction}"
+end
+
+puts "Guests' diet restrictions after .each:"
+print guests_diet_restrictions
+
+
+#use .map! to modify the pasta ingredients array
+puts "Pasta ingredients before .map!"
+print pasta_ingredients
+
+pasta_ingredients.map! do |ingredient|
+  "delicious #{ingredient}"
+end
+
+puts "Pasta ingredients after .map!"
+print pasta_ingredients
