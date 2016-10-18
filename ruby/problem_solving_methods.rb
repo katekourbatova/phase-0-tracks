a6e8c7b7d15f59d5da862c9c33169cba87ccbe72
Kate Kourbatova <ekateko@uw.edu>
2016-10-17 19:15:46 -0700

    Add pseudocode for insertion sort method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -49,3 +49,15 @@ p generate_fibonacci(6)
 puts "Is the 100th term in Fibonacci sequence correctly generated?"
 correct_100_term = (generate_fibonacci(100)[99] == 218922995834555169026)
 p correct_100_term
+
+
+# Pseudocode insertion sort method:
+# Start with the second item (index 1) in the list, and assign the value of that item to a "key".
+# Compare the key to the current index-1 (in this case 0).
+# - If the key is bigger, leave it in place.
+# - If the key is smaller, move the value you're comparing over to the right (move index 0 to index 1),
+#   and then copy the value of the key into the space to the left of the value that is larger than it.
+# - move to the next index, repeat while the index is less than the length of the array.
+#   (except this time, keep comparing to each index to the left until you find one that is not smaller)
+
+
