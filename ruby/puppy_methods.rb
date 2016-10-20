dd8a2a0cfd54e66056bf6af68af6a25e31c1fcbc
Kate Kourbatova <ekateko@uw.edu>
2016-10-20 14:00:34 -0700

    Add methods to puppy class, and add driver code to test them.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
+  def speak(n)
+    n.times do
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    puts human_years*7
+  end
+
+  def bite(item)
+    puts "I bit the #{item}! It was great!"
+  end
 end
 
+Spot = Puppy.new
+Spot.fetch("ball")
+Spot.speak(3)
+Spot.roll_over
+Spot.dog_years(2.5)
+Spot.bite("flute")
