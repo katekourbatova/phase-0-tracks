24ab98dd1ba71f56b2fb9c93d6d6f6cf2241683b
Kate Kourbatova <ekateko@uw.edu>
2016-10-22 22:35:58 -0700

    Add nested data structure of animals

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,21 @@
+animals = {
+  felines: ["cougars", "lions", "cats and kittens"]
+  canines: {
+    domestic_dogs: {
+      behaviors: [
+        "running",
+        "fetching balls",
+        "licking people",
+        "barking"],
+      typical_names: [
+        "Spot",
+        "Fido",
+        "Clifford",
+        "Rex"]
+    },
+    wolves: ["Gray wolf", "Eastern wolf"],
+    foxes: ["Red fox", "Bat-eared fox", "Gray fox"],
+    jackals: ["Black-backed jackal", "Side-striped jackal", "Golden jackal"]
+    other_species: ["Coyote"]
+  }
+}
