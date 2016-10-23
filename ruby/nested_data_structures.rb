aa95780befd6c129656a90cfe6df91c1bbc3e574
Kate Kourbatova <ekateko@uw.edu>
2016-10-22 22:52:34 -0700

    Add calls using nested data in various ways.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,5 +1,5 @@
 animals = {
-  felines: ["cougars", "lions", "cats and kittens"]
+  felines: ["cougars", "lions", "cats and kittens"],
   canines: {
     domestic_dogs: {
       behaviors: [
@@ -15,7 +15,18 @@ animals = {
     },
     wolves: ["Gray wolf", "Eastern wolf"],
     foxes: ["Red fox", "Bat-eared fox", "Gray fox"],
-    jackals: ["Black-backed jackal", "Side-striped jackal", "Golden jackal"]
+    jackals: ["Black-backed jackal", "Side-striped jackal", "Golden jackal"],
     other_species: ["Coyote"]
   }
 }
+
+puts "A #{animals[:canines][:other_species][0]} is a type of canine."
+
+dog_names = animals[:canines][:domestic_dogs][:typical_names]
+dog_behaviors = animals[:canines][:domestic_dogs][:behaviors]
+
+puts "#{dog_names[0]} enjoys #{dog_behaviors[-1]}."
+puts "His friend #{dog_names[3]} prefers #{dog_behaviors[1]} and #{dog_behaviors[2]}."
+
+puts "Felines include:"
+puts animals[:felines]
