7786993269f7c8fe8f8ae8ad6a128a6c399fe656
Kate Kourbatova <ekateko@uw.edu>
2016-10-23 23:26:20 -0700

    Add method for creating a fake name, and short driver code to test.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,3 +14,36 @@
 
 # 7. Join the character array back into a string, capitalize it.
 # Output: Fake name string
+
+def create_alias(real_name)
+
+  vowels = ["a", "e", "i", "o", "u"]
+  consonants = ["b", "c", "d", "f", "g", "h", "j",
+  "k", "l", "m", "n", "p", "q", "r", "s", "t",
+  "v", "w", "x", "y", "z"]
+
+  # takes the real name, splits into words, reverses the order, joins into string again, and lowercases
+  reversed_lowcase_str = real_name.split(" ").reverse.join(" ").downcase
+  name_char_array = reversed_lowcase_str.chars
+  fake_name_char_arr = name_char_array.map! do |letter|
+    if letter == " "
+     letter
+    elsif letter == "u"
+      letter = "a"
+    elsif letter == "z"
+      letter = "b"
+    elsif vowels.include?(letter)
+      letter = vowels[vowels.index(letter) + 1] # assigns the letter to be the next vowel
+    elsif consonants.include?(letter)
+      letter = consonants[consonants.index(letter) + 1]
+    end
+  end
+
+# rejoins the character array into one string, splits it into word array,
+# capitalizes each word, rejoins into string
+  fake_name = fake_name_char_arr.join("").split(" ").map!{|name| name.capitalize}.join(" ")
+end
+
+
+p create_alias("Kate Kourbatova")
+
