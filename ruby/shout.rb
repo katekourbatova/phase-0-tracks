f4443e2ed9f216eff21870eb60826bf27d96a5e8
Kate Kourbatova <ekateko@uw.edu>
2016-10-24 23:07:42 -0700

    Make a mixin version of Shout module, and include it in Toddler and Trump classes.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,37 @@
+# Old standalone module
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words.upcase + "!!! :D :D"
+#   end
+# end
+
+# p Shout.yell_angrily("I am unhappy")
+# p Shout.yell_happily("I am happy")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    puts words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
-    words.upcase + "!!! :D :D"
+  def yell_happily(words)
+    puts words.upcase + "!!! :D :D"
   end
 end
 
-p Shout.yell_angrily("I am unhappy")
-p Shout.yell_happily("I am happy")
+class Toddler
+  include Shout
+end
+
+class Trump
+  include Shout
+end
+
+toddler_amy = Toddler.new
+toddler_amy.yell_happily("There's a bird outside")
+
+the_donald = Trump.new
+the_donald.yell_angrily("The election is rigged")
