ae47372d83b92104d461fc3dbf9482c6e6b13f9a
Kate Kourbatova <ekateko@uw.edu>
2016-10-30 11:27:52 -0700

    Add changes made before I was driver -- colors and horse names arrays.

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["Lime green", "Magenta", "Cyan", "Chartreuse"];
+
+var horseNames = ["Ed", "Sprinkles", "Unicorn", "Cerulian"];
+
+colors.push("Brick red");
+horseNames.push("Buster");
+
+console.log(colors);
+console.log(horseNames);
