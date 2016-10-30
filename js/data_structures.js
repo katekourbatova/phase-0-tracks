5f57e0dbadaaea10610a9cb237834c1bdb5a870f
Kate Kourbatova <ekateko@uw.edu>
2016-10-30 12:39:22 -0700

    Add Horses object that pairs horse names and colors, also commented Horse object that assigns a name and a color but doesn't pair them.

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,21 @@ horseNames.push("Buster");
 
 console.log(colors);
 console.log(horseNames);
+
+// function Horse(index) {
+//   this.name = horseNames[index];
+//   this.color = colors[index];
+//   console.log("Our new horse is named " + this.name + " and is a " + this.color + " color.");
+// }
+
+// var newHorse = new Horse(0);
+// console.log(newHorse);
+
+function Horses(index) {
+  var horse = {}
+  horse[horseNames[index]] = colors[index];
+  console.log(horse);
+}
+
+var pairedHorse = new Horses(1);
+var pairedHorse = new Horses(colors.length - 1);
