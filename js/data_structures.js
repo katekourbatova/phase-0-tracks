7d4e1df2edd26a1540c141426b95f4303886ad41
Kate Kourbatova <ekateko@uw.edu>
2016-10-30 12:53:32 -0700

    Add car object that takes a color, model, year, and mileage, and has functions for accelerating and printing info about itself.

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,6 +8,7 @@ horseNames.push("Buster");
 console.log(colors);
 console.log(horseNames);
 
+// Unpaired Horse
 // function Horse(index) {
 //   this.name = horseNames[index];
 //   this.color = colors[index];
@@ -17,6 +18,8 @@ console.log(horseNames);
 // var newHorse = new Horse(0);
 // console.log(newHorse);
 
+
+// Paired horses (name/color value pairs)
 function Horses(index) {
   var horse = {}
   horse[horseNames[index]] = colors[index];
@@ -25,3 +28,22 @@ function Horses(index) {
 
 var pairedHorse = new Horses(1);
 var pairedHorse = new Horses(colors.length - 1);
+
+// Car
+function Cars(color, model, year, mileage) {
+  this.color = color;
+  this.model = model;
+  this.year = year;
+  this.mileage = mileage;
+  this.accelerate = function() { console.log("Going faster and faster!"); };
+  this.about = function() { console.log ("Color: " + color + "\nModel: " + model); };
+
+}
+
+var newCar = new Cars("red", "sedan", 2012, 50000);
+newCar.about();
+
+var secondCar = new Cars("green", "Jeep", 1991, 1000000);
+secondCar.about();
+secondCar.accelerate();
+
