4f078a428ec4c93bcf1831a880f424e1140854c6
Kate Kourbatova <ekateko@uw.edu>
2016-10-30 02:58:38 -0700

    Adjust spec file and finish class and built in methods

--- ruby/6-6-word-game/wordgame.rb
+++ ruby/6-6-word-game/wordgame.rb
@@ -1,49 +1,100 @@
 class SecretWordGame
-  attr_reader :you_won, :you_lost
-  attr_accessor :prev_guesses
+  attr_reader :you_won, :you_lost, :reveal_word_str, :n_guesses_left, :secret_word_str
 
   def initialize
-    # @prev_guesses = {}
-    # @you_won = false
-    # @you_lost = false
-    # @n_allowed_guesses = nil
-    # @secret_word_str = ""
-    # @reveal_word_str = ""
+    @prev_guesses = []
+    @you_won = false
+    @you_lost = false
+    @n_guesses_left = nil
+    @secret_word_str = ""
+    @reveal_word_str = ""
   end
 
-  def secret_word(word)
-    # @secret_word_str = word
-    # @n_allowed_guesses = word.length + 5
-    # @reveal_word_str = ???
+  def set_secret_word(word)
+    @secret_word_str = word
+    @n_guesses_left = word.length + 3
+    @reveal_word_str = ("_" * word.length)
   end
 
   def check_guess(guess)
+    if !(@prev_guesses.include? guess)
+      @prev_guesses << guess
+      @n_guesses_left -= 1
+    end
 
+    if guess == @secret_word_str
+      @reveal_word_str = @secret_word_str
+      @you_won = true
+    
+    #if the guess is 1 char long, and the secret word includes the guess
+    elsif (@secret_word_str.include? guess) && (guess.length == 1)
+      #for each index in secret word, check if character is equal to guess, adjust revealed word string as necessary
+      @secret_word_str.chars.each_index do |charindex|
+        if @secret_word_str[charindex] == guess
+          @reveal_word_str[charindex] = guess
+        end
+      end
+    end
+
+    if !(@reveal_word_str.include? "_")
+      @you_won = true
+    end
+
+    if @n_guesses_left == 0
+      @you_lost = true
+    end
   end
-
 end
 
+#Driver code:
+
+# puts "Welcome to our word guessing game!"
+# game = SecretWordGame.new
+
+# puts "Please set a word you'd like your friend to guess."
+# the_word = gets.chomp
+
+# game.set_secret_word(the_word)
+
+# puts "Now invite a friend to guess your word."
+
+# puts "Hello, new player! Your friend has set a secret word for you to guess."
+
+
+# until game.you_won || game.you_lost
+#   puts "Please type in a letter you think is in the word, or the whole word if you think you know it. You have #{game.n_guesses_left} guesses."
+#   guess = gets.chomp
+#   game.check_guess(guess)
+#   puts "The word so far: #{game.reveal_word_str}"
+# end
+
+# if game.you_won
+#   puts "Way to go! You won with #{game.n_guesses_left} guesses left."
+# elsif game.you_lost
+#   puts "You lost.... better luck next time."
+# end
+
+
+
+
 # The class is WordGame.
-# The user should be able to access (read and write) the previous guesses (hash) attribute
+# The user should be able to access (read and write) the previous guesses (array) attribute
 # The user should be able to read the ‘you won’ attribute, and ‘you lost’ attribute
 
 # The initialize method
-# Empty previous guesses hash
+# Empty previous guesses array
 # You lost is false
 # You won is false
-# Allowed guesses is nil
+# Guesses left is nil
 # Secret word string is empty
-# Secret word array is empty
-# Revealed word array is empty
+# Revealed word string is empty
 
 # The secret_word method
-# Breaks word up into array of characters
-# Set number of allowed guesses to 5 + the length of the word
-# Revealed word array is equal in length to word array, but has underscores only
+# Set number of allowed guesses to 3 + the length of the word
+# Revealed word string is equal in length to word string, but has underscores only
 
 # The check_guess method
-# UNTIL guess hash length is equal to number of allowed guesses
-# store guess in the guess hash
+# store guess in the guess array
 # check if the string entered corresponds to the secret word string 
 #   if guessed correctly, you_won is true
 # if you_won is false and user entered a 1-character string
--- ruby/6-6-word-game/wordgame_spec.rb
+++ ruby/6-6-word-game/wordgame_spec.rb
@@ -4,19 +4,17 @@ describe SecretWordGame do
   let(:secretwordgame) { SecretWordGame.new }
   
   it "assigns secret word to string variable" do
-    expect(secretwordgame.secret_word("coconut")).to change{SecretWordGame.secret_word_str}.from("").to("coconut")
+    expect {secretwordgame.set_secret_word("coconut")}.to change{secretwordgame.secret_word_str}.from("").to("coconut")
   end
 
-  it "assigns secret word to array" do
-    expect(secretwordgame.secret_word("coconut")).to change{SecretWordGame.secret_word_arr}.from([]).to(["c", "o", "c", "o", "n", "u", "t"])
-  end
-
-  it "assigns allowed number of guesses to be the word length plus 5" do
-    expect(secretwordgame.secret_word("coconut")).to change{SecretWordGame.n_allowed_guesses}.from(nil).to(13)
+  it "assigns allowed number of guesses to be the word length plus 3" do
+    expect {secretwordgame.set_secret_word("coconut")}.to change{secretwordgame.n_guesses_left}.from(nil).to(10)
   end
 
   it "assigns revealed word array to be the corresponding number of underscores" do
-    expect(secretwordgame.secret_word("coconut")).to change{SecretWordGame.n_allowed_guesses}.from(nil).to(13)
+    expect {secretwordgame.set_secret_word("coconut")}.to change{secretwordgame.reveal_word_str}.from("").to("_______")
   end
+
+
 
 end
