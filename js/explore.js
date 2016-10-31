d8dd5c814777b45355eb4c3392afd227794e4c8a
Kate Kourbatova <ekateko@uw.edu>
2016-10-31 08:21:28 -0700

    Add pseudocode for function to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Function to reverse a string
+// The function will take a string
+// It will take characters one by one from the end of the string
+// And add them onto a reversed string
+// Then return the reversed string
+
