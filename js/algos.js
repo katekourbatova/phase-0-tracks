0e0dbf6a00d93bade7a45c8d951f0a913d3b9a5a
Kate Kourbatova <ekateko@uw.edu>
2016-11-04 01:07:59 -0700

    Debug function for creating array of random strings.

--- js/algos.js
+++ js/algos.js
@@ -37,14 +37,14 @@ function match_pair(obj1, obj2) {
 // (loop) FOR numbers 0 to n-1 assign a string to that number in array
 // String should be a random number (1-10) of random letters
 
-function build_string_array(n) {
+function build_string_array(n_words) {
   var new_array = [];
   var n_letters = 0;
   var alphabet = "abcdefghijklmnopqrstuvwxyz";
-  for (var i = 0; i < n; i++) {
-    n_letters = (Math.floor(Math.random() * 11); // Math.random generates between 0(inclusive) and 1(exclusive). *10 would make it up to 10(exclusive). Add one, then floor. Result is whole number between 1 and 10, inclusive.
+  for (var i = 0; i < n_words; i++) {
+    n_letters = (Math.floor(Math.random() * 10) + 1); // Math.random generates between 0(inclusive) and 1(exclusive). *10 would make it up to 10(exclusive). Add one, then floor. Result is whole number between 1 and 10, inclusive.
     var new_string = "";
-    for (var 1=0, i<n_letters, i++) {
+    for (var x = 0; x < n_letters; x++) {
       new_string += alphabet.charAt(Math.floor(Math.random()*27)); //adds to new string a random character from alphabet string. 
     }
     new_array[i] = new_string;
@@ -70,7 +70,8 @@ console.log(match_pair({name: "Steven", age: 54}, {name: "Tamir", age: 65}));
 console.log("3. All different keys, no matching values");
 console.log(match_pair({name: "Steven", age: 54}, {nickname: "Tamir", catname: "Maggie Scratcher"}));
 
-
-
+console.log("Testing function for creating array of random strings.");
+console.log(build_string_array(2));
+console.log(" ");
 
 
